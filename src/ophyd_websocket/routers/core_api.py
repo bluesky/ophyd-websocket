dd68d6967cff62a6a2c1bc96716d4753fda668ce
seijdeleon <seijdeleon@lbl.gov>
2026-08-21 16:27:24 -0700

    use correct instruction property in message

--- src/ophyd_websocket/routers/core_api.py
+++ src/ophyd_websocket/routers/core_api.py
@@ -191,10 +191,10 @@ async def set_device_value(instruction: OphydDeviceInstruction, response: Respon
             set_result.wait(timeout=instruction.timeout)
             return {
                 "success": True,
-                "message": f"Successfully set {target_name} to {instruction.value} (with timeout {instruction.timeout}s)",
+                "message": f"Successfully set {target_name} to {instruction.set_value} (with timeout {instruction.timeout}s)",
                 "device": instruction.device,
                 "component": instruction.component,
-                "value": instruction.value,
+                "value": instruction.set_value,
                 "timeout": instruction.timeout
             }
         else:
